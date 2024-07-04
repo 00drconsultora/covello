d2272e2c106eacb869087a5ccedc044e8f0b07ce
lucasgonzalezm <lucasgonzalezm3@gmail.com>
2024-07-04 15:32:44 -0300

    href base href var

---
+++ src/set-base-href.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+// Get the base href from the command line arguments
+const baseHref = process.argv[2] || '/';
+
+// Define the path to the index.html file in the dist folder
+const indexPath = path.join(__dirname, 'dist/landing-page/index.html');
+
+// Read the index.html file
+fs.readFile(indexPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading index.html:', err);
+    return;
+  }
+
+  // Replace the %BASE_HREF% placeholder with the actual base href
+  const updatedData = data.replace(/%BASE_HREF%/g, baseHref);
+
+  // Write the updated data back to the index.html file
+  fs.writeFile(indexPath, updatedData, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing index.html:', err);
+    } else {
+      console.log('Successfully updated base href in index.html');
+    }
+  });
+});
