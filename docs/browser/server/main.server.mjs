67759e0633f394876b5e0b9027134791eb375887
lucasgonzalezm <lucasgonzalezm3@gmail.com>
2024-07-11 14:17:33 -0300

    Update to dynamically set base URL

--- docs/browser/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-BCQPZADH.mjs";import"./chunk-LZU2TYKG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CLD3JSVO.mjs";import"./chunk-QLTH3LGK.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
