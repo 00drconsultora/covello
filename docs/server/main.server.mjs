0df55c39f6efd98f1c4390efa04adcb4edf8053b
lucasgonzalezm <lucasgonzalezm3@gmail.com>
2024-07-12 09:57:37 -0300

    testing deployment with just one picture in app.component with this format <img src="covello/assets/renders/Postal_Carmelo_R2.jpg">

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KAFDTB75.mjs";import"./chunk-MHT6OTGG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-6Z7ZK2JY.mjs";import"./chunk-MHT6OTGG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
